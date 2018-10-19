0b5ac8bf683b14f4dc479130b100590e7434c123
Ben <ben399399@naver.com>
2018-10-19 20:07:52 +0900

    Retarget Dazzling

--- dazzling/dazzling/lib/main.dart
+++ dazzling/dazzling/lib/main.dart
@@ -1,143 +1,59 @@
 import 'package:flutter/material.dart';
-import 'theme.dart';
 
 void main() => runApp(Dazzling());
 
-class Dazzling extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Dazzling that Dazzles',
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.blue,
-      ),
-      home: DazzlingPage(),
-    );
-  }
-}
-
-class DazzlingPage extends StatefulWidget {
+class Dazzling extends StatefulWidget {  
   @override
   DazzlingStatus createState() => new DazzlingStatus();
 }
 
-class DazzlingStatus extends State<DazzlingPage> {
+class DazzlingStatus extends State<Dazzling> {
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        title: Text(''),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios),
-          color: const Color(0xFFDDDDDD),
-          onPressed: () {},
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            color: const Color(0xFFDDDDDD),
-            onPressed: () {},
-          )
-        ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title:'Dazzling that Dazzles',
+      theme: ThemeData(
+        primaryColor: Color(0xFF344955),
+        accentColor: Color(0xFFF9AA33)
       ),
-      body: Column(
-        children: <Widget>[
-          //seekbar
-          Expanded(
-            child: Center(
-              child: Container(
-                width: 125.0,
-                height: 125.0,
-                child: Image.network(src),
-              ),
+      home: Scaffold(
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              setState(() {
+  
+              });
+              
+              
+              
+            },
+            
+          ),
+          bottomNavigationBar: BottomAppBar(
+            notchMargin: 8.0,
+            shape: CircularNotchedRectangle(),
+            color: Color(0xFF344955),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.list),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.search),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+              ],
             ),
           ),
-          //visualizer
-          Container(
-            width: double.infinity,
-            height: 125.0,
-          ),
-          //song title, artist,etc
-          Container(
-            color: lightAccentColor,
-            child: Padding(
-              padding: const EdgeInsets.only(top:40.0,bottom: 50.0),
-                child: Column(
-                children: <Widget>[
-                  RichText(
-                    text: TextSpan(
-                      text: '', children: [
-                        TextSpan(
-                          text: 'Song title\n',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 4.0,
-                            height: 1.5,
-                          )
-                        ),
-                        TextSpan(
-                          text: 'Artist Name\n',
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.75),
-                            fontSize: 12.0,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 3.0,
-                            height: 1.5,
-                          )
-                        ),
-                  ])),
-                  Padding(
-                    padding: const EdgeInsets.only(top:40.0),
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(child:Container()),
-                        IconButton(
-                          color: Colors.white,
-                          icon: Icon(Icons.skip_previous,size: 35.0,),
-                          onPressed: () {},
-                        ),
-                        Expanded(child:Container()),
-                        
-                        RawMaterialButton(
-                          shape: CircleBorder(),
-                          fillColor: Colors.white,
-                          splashColor: lightAccentColor,
-                          highlightColor: lightAccentColor.withOpacity(0.5),
-                          elevation: 10.0,
-                          highlightElevation: 5.0,
-                          onPressed: (){},
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Icon(
-                              Icons.play_arrow,
-                              color: darkAccentColor,
-                              size: 35.0
-                            ),
-                          ),
-                        ),
-
-                        Expanded(child:Container()),
-                        IconButton(
-                          color: Colors.white,
-                          icon: Icon(Icons.skip_next,size: 35.0),
-                          onPressed: () {},
-                        ),
-                        Expanded(child:Container()),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
+          body: Container()),
     );
   }
 }
